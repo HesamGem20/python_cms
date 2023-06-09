97f1e44880bc6fe82c7048fb9ef0163bd641a9ba
Navid Taheri <navidtaheri@gmail.com>
2023-06-09 21:56:15 +0200

    rev_3_modules commits

--- models.py
+++ models.py
@@ -1,8 +1,16 @@
+"""
+This module defines the database models for the blog posts.
+"""
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Post(db.Model):
+    """
+    Represents a blog post.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
