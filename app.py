1ed8d8b6af2e1680fa4f3c27a9159e4e13e5d23e
Navid Taheri <navidtaheri@gmail.com>
2023-06-09 21:55:12 +0200

    rev_2

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
+"""
+This is a Flask web application that allows users to create and edit blog posts.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Renders the homepage template.
+    """
     return render_template('index.html')
 
 @app.route('/create_post')
 def create_post():
+    """
+    Renders the create post template.
+    """
     return render_template('create_post.html')
 
 @app.route('/edit_post/<int:post_id>')
 def edit_post(post_id):
+    """
+    Renders the edit post template with the specified post ID.
+    """
     return render_template('edit_post.html', post_id=post_id)
 
 if __name__ == '__main__':
